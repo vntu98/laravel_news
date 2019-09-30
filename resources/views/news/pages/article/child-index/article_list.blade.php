89e78546a2daad1f703e64f54ae036a8cff4a177
vutu <vntu98@gmail.com>
2019-09-30 13:52:51 +0700

    fix html page home

--- resources/views/news/pages/article/child-index/article_list.blade.php
+++ resources/views/news/pages/article/child-index/article_list.blade.php
@@ -6,7 +6,7 @@
                     @include('news.partials.article.image', ['item' => $article])
                 </div>
                 <div class="col-lg-7">
-                    @include('news.partials.article.content', ['item' => $article, 'lengthContent' => 200, 'showCategory' => false])
+                    @include('news.partials.article.content', ['item' => $article, 'lengthContent' => 150, 'showCategory' => false])
                 </div>
             </div>
         </div>
